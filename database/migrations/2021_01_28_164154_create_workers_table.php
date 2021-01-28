1737d092e3935fabd4ecc2a5ed4544b715158382
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2021-01-28 17:53:56 +0100

    terminato procedimento di migration

---
+++ database/migrations/2021_01_28_164154_create_workers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('name', 50);
+            $table->string('lastname', 60);
+            $table->date('hire_date');
+
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}
